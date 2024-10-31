c2a68bcc84f0d46ccd75da0c5b96e98a18e159cd
Ratchanon <ratchanon162040@gmail.com>
2024-10-31 23:28:22 +0700

    upgrade code

--- navbar.js
+++ navbar.js
@@ -1,6 +1,7 @@
+// navbar
 document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function() {
-      document.querySelectorAll('.nav-link').forEach(nav => nav.classList.remove('active'));
-      this.classList.add('active');
-    });
-  });
+	link.addEventListener('click', function () {
+		document.querySelectorAll('.nav-link').forEach(nav => nav.classList.remove('active'));
+		this.classList.add('active');
+	});
+});
